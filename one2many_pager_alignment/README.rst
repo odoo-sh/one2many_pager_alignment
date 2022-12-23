10e7b5d355a075e092b766804e9327d251953cf0
Sandeep, Sodexis <sandeep@sodexis.com>
2022-12-23 11:36:24 +0530

    [ID:27059] View corrections (#7137)

---
+++ one2many_pager_alignment/README.rst
@@ -0,0 +1,11 @@
+========================
+One2many Pager Alignment
+========================
+
+This module is used to Align the pager of one2many fields.
+
+
+Contributors
+------------
+
+* Sodexis
---
+++ one2many_pager_alignment/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Sodexis
+# License OPL-1 (See LICENSE file for full copyright and licensing details).
+{
+    "name": "One2many Pager Alignment",
+    "summary": """This module is used to Align the pager of one2many fields""",
+    "version": "14.0.1.0.0",
+    "category": "Tools",
+    "website": "http://sodexis.com/",
+    "author": "Sodexis",
+    "license": "OPL-1",
+    "installable": True,
+    "application": False,
+    "depends": [
+        'web'
+    ],
+    "data": [
+        "views/assets.xml",
+    ],
+}
